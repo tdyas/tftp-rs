6a75e330aef0f9dd920259313f95bbc1e9d5337e
Tom Dyas <tom@shoalsoftware.com>
2018-01-26 12:39:36 -0800

    add unit testing framework for the TFTP server
    The unit tests for the TFTP server are a very simple packet simulator.
    
    Issues:
    - The simulator looks for an exact match on the packets. The TFTP option
      handling will need to sort the options to ensure reproducible results.
      (Once of course this support is actually written.)
    
    - Testing of the timeout behavior not yet implemented.
    
    - Testing for working requests will need some support in `TftpServer` to
      not hit the filesystem to find files. (I.e., the test framework needs to
      mock out the fileystem.)

--- src/server.rs
+++ src/server.rs
@@ -19,6 +19,7 @@ use super::udp_stream::{Datagram, RawUdpStream};
 
 pub struct TftpServer {
     incoming: Box<Future<Item=(), Error=io::Error>>,
+    local_addr: SocketAddr,
 }
 
 struct ReadState {
@@ -153,12 +154,18 @@ impl TftpServer {
 
         let socket = UdpSocket::bind(addr, &handle)?;
         let stream = RawUdpStream::new(socket);
+        let local_addr = stream.local_addr().unwrap();
 
         let incoming = TftpServer::main_loop(stream, Rc::new(root), handle.clone());
 
         Ok(TftpServer {
             incoming: incoming,
+            local_addr: local_addr,
         })
+    }
+
+    pub fn local_addr(&self) -> &SocketAddr {
+        &self.local_addr
     }
 }
 
@@ -167,5 +174,168 @@ impl Future for TftpServer {
     type Error = io::Error;
     fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
         self.incoming.poll()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::cell::RefCell;
+    use std::error::Error;
+    use std::fmt;
+    use std::io;
+    use std::net::SocketAddr;
+    use std::path::Path;
+
+    use bytes::{Bytes, BytesMut};
+    use futures::prelude::*;
+    use tokio_core::net::UdpSocket;
+    use tokio_core::reactor::{Core, Handle};
+
+    use super::TftpServer;
+    use super::super::proto::TftpPacket;
+    use super::super::udp_stream::{Datagram, RawUdpStream};
+
+    enum Op {
+        Send(Bytes),
+        Receive(Bytes),
+    }
+
+    struct TestContext {
+        main_remote_addr: SocketAddr,
+        remote_addr_opt: Option<SocketAddr>,
+        sink: Option<Box<Sink<SinkItem=Datagram, SinkError=io::Error>>>,
+        stream: Option<Box<Stream<Item=Datagram, Error=io::Error>>>,
+    }
+
+    #[derive(Debug)]
+    struct TestError(String);
+
+    impl TestError {
+        fn new(message: &str) -> TestError {
+            TestError(message.to_owned())
+        }
+    }
+
+    impl fmt::Display for TestError {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            self.0.fmt(f)
+        }
+    }
+
+    impl Error for TestError {
+        fn description(&self) -> &str {
+            self.0.as_str()
+        }
+    }
+
+    #[async]
+    fn test_driver(context: Box<RefCell<TestContext>>, steps: Vec<Op>) -> Result<(), TestError> {
+        for step in steps {
+            match step {
+                Op::Send(bytes) => {
+                    let main_remote_addr = context.borrow().main_remote_addr;
+                    let remote_addr_opt = context.borrow().remote_addr_opt;
+                    let remote_addr = remote_addr_opt.unwrap_or(main_remote_addr);
+
+                    let datagram = Datagram { addr: remote_addr, data: bytes };
+
+                    let sink = context.borrow_mut().sink.take().unwrap();
+                    let result = await!(sink.send(datagram));
+                    match result {
+                        Ok(sink) => {
+                            context.borrow_mut().sink = Some(Box::new(sink));
+                        },
+                        Err(err) => {
+                            return Err(TestError::new(err.description()));
+                        },
+                    }
+                },
+                Op::Receive(bytes) => {
+                    let stream = context.borrow_mut().stream.take().unwrap();
+                    let result = await!(stream.into_future());
+
+                    let datagram_opt = match result {
+                        Ok((datagram_opt, stream)) => {
+                            context.borrow_mut().stream = Some(Box::new(stream));
+                            datagram_opt
+                        },
+                        Err((err, stream)) => {
+                            context.borrow_mut().stream = Some(Box::new(stream));
+                            return Err(TestError::new(err.description()));
+                        },
+                    };
+
+                    match datagram_opt {
+                        Some(datagram) => {
+                            if context.borrow().remote_addr_opt.is_none() {
+                                context.borrow_mut().remote_addr_opt = Some(datagram.addr);
+                            }
+                            match TftpPacket::from_bytes(&datagram.data) {
+                                Ok(packet) => {
+                                    println!("Received {}", &packet);
+                                },
+                                Err(err) => return Err(TestError::new(err.description())),
+                            };
+
+                            if bytes != datagram.data {
+                                return Err(TestError::new("Packets differ"));
+                            }
+                        },
+                        None => {
+                            return Err(TestError::new("Stream terminated early."))
+                        },
+                    }
+                },
+            }
+        }
+
+        Ok(())
+    }
+
+    fn do_test(server_addr: &SocketAddr, handle: Handle, steps: Vec<Op>) -> Box<Future<Item=(), Error=TestError>> {
+        let addr = "0.0.0.0:0".parse().unwrap();
+        let socket = UdpSocket::bind(&addr, &handle).unwrap();
+        let stream = RawUdpStream::new(socket);
+        let (sink, stream) = stream.split();
+
+        let context = Box::new(RefCell::new(TestContext {
+            main_remote_addr: server_addr.clone(),
+            remote_addr_opt: None,
+            sink: Some(Box::new(sink)),
+            stream: Some(Box::new(stream)),
+        }));
+
+        Box::new(test_driver(context, steps))
+    }
+
+    fn mk(packet: TftpPacket) -> Bytes {
+        let mut buffer = BytesMut::with_capacity(1024);
+        packet.encode(&mut buffer);
+        buffer.freeze()
+    }
+
+    fn run_test(core: &mut Core, server_addr: &SocketAddr, test_name: &str, steps: Vec<Op>) {
+        let test_driver = do_test(server_addr, core.handle(), steps);
+        let result = core.run(test_driver);
+        if let Err(err) = result {
+            panic!("Test failed ({}): {}", test_name, err);
+        }
+    }
+
+    #[test]
+    fn test_read_requests() {
+        let mut core = Core::new().unwrap();
+        let handle = core.handle();
+
+        let server_addr: SocketAddr = "0.0.0.0:0".parse().unwrap();
+        let server = TftpServer::bind(&server_addr, handle.clone(), Path::new(".")).unwrap();
+        let server_addr = server.local_addr().clone();
+
+        handle.spawn(server.map_err(|_| ()));
+
+        run_test(&mut core, &server_addr, "file not found", vec![
+            Op::Send(mk(TftpPacket::ReadRequest { filename: b"missing", mode: b"octet" })),
+            Op::Receive(mk(TftpPacket::Error { code: 1, message: b"File not found"})),
+        ]);
     }
 }
