873d2af2cea6b216d4277ed39a2481a633126851
Tom Dyas <tom.dyas@gmail.com>
2019-11-26 11:44:28 -0800

    remove use of pin-utils in examples/server

--- examples/server.rs
+++ examples/server.rs
@@ -1,17 +1,13 @@
-extern crate tftp_rs;
-
-use pin_utils::pin_mut;
 use tftp_rs::TftpServer;
 
 #[tokio::main]
 async fn main() {
-    let server = TftpServer::builder()
+    let mut server = TftpServer::builder()
         .port(9090)
         .read_root(".")
         .write_root(".")
         .build()
         .await
         .unwrap();
-    pin_mut!(server);
     let _ = server.main_loop().await;
 }
